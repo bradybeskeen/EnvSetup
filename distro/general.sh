667e3f2bef73e86375f94b988246423bdaecebaf
bradybeskeen <bradonbeskeen@gmail.com>
2025-09-22 22:01:25 -0700

    Added lua and node installs to distro setup
    * init.lua - removed more comments after I did suffiencient research on
    the area
    * general.sh - functionized content and make functions for installing
    lua and node

--- distro/general.sh
+++ distro/general.sh
@@ -1,10 +1,55 @@
+#!/bin/sh
 # Distro Agnostic Functions
 
 # Install Go Mono Nerf Font
-wget -O gomono_nerd.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Go-Mono.zip
-unzip -d gomono_nerd gomono_nerd.zip
-mkdir -p $HOME/.local/share/fonts
-cp gomono_nerd/*.ttf $HOME/.local/share/fonts/
-rm -rf gomono_nerd*
-fc-cache -f
-echo "Restart your terminal to apply"
+install_nerd_font() {
+	wget -O gomono_nerd.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Go-Mono.zip
+	unzip -d gomono_nerd gomono_nerd.zip
+	mkdir -p $HOME/.local/share/fonts
+	cp gomono_nerd/*.ttf $HOME/.local/share/fonts/
+	rm -rf gomono_nerd*
+	fc-cache -f
+	echo "Restart your terminal to apply"
+}
+
+install_node_js() {
+	# Download and install nvm:
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+	# in lieu of restarting the shell
+	\. "$HOME/.nvm/nvm.sh"
+
+	# Download and install Node.js:
+	nvm install 22
+
+	# Verify the Node.js version:
+	node -v # Should print "v22.19.0".
+
+	# Verify npm version:
+	npm -v # Should print "10.9.3".
+}
+
+install_lua() {
+	# Get Lua from remote
+	curl -L -R -O https://www.lua.org/ftp/lua-5.4.8.tar.gz
+	tar zxf lua-5.4.8.tar.gz
+	cd lua-5.4.8
+	sudo make install
+	cd ..
+
+	# Get LuaRocks from remote
+	curl -L -R -O https://luarocks.org/releases/luarocks-3.12.2.tar.gz
+	tar zxpf luarocks-3.12.2.tar.gz
+	cd luarocks-3.12.2
+	./configure --lua-version=5.4 --with-lua-bin=/usr/local/bin
+	make && sudo make install
+	cd ..
+
+	rm -rf lua-5.4.8.tar.gz luarocks-3.12.2.tar.gz
+	sudo mv -f lua-5.4.8 luarocks-3.12.2 /opt
+}
+
+# TODO: One day call these conditionally only if not already in system
+install_nerd_font
+install_node_js
+install_lua
--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -597,43 +597,9 @@ require('lazy').setup {
       'saghen/blink.cmp',
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc, mode)
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -641,23 +607,23 @@ require('lazy').setup {
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('grn', vim.lsp.buf.rename, 'Rename')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+          map('gra', vim.lsp.buf.code_action, 'Goto Code Action', { 'n', 'x' })
 
           --Find references for the word under your cursor.
-          map('grr', require('fzf-lua').lsp_references, '[G]oto [R]eferences')
+          map('grr', require('fzf-lua').lsp_references, 'Goto References')
 
           -- Jump to the implementation of the word under your cursor.
-          map('gri', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
+          map('gri', require('fzf-lua').lsp_implementations, 'Goto Implementation')
 
           -- Jump to the definition of the word under your cursor.
-          map('grd', require('fzf-lua').lsp_definitions, '[G]oto [D]efinition')
+          map('grd', require('fzf-lua').lsp_definitions, 'Goto Definition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
-          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('grD', vim.lsp.buf.declaration, 'Goto Declaration')
 
           -- Fuzzy find all the symbols in your current document.
           map('gO', require('fzf-lua').lsp_document_symbols, 'Open Document Symbols')
@@ -666,7 +632,7 @@ require('lazy').setup {
           map('gW', require('fzf-lua').lsp_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
-          map('grt', require('fzf-lua').lsp_typedefs, '[G]oto [T]ype Definition')
+          map('grt', require('fzf-lua').lsp_typedefs, 'Goto Type Definition')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
@@ -737,36 +703,11 @@ require('lazy').setup {
           end,
         },
       }
-
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+      --  Create new capabilities with blink.cmp, and then broadcast that to the servers.
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      -- See `:help lspconfig-all`
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -776,16 +717,23 @@ require('lazy').setup {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
+        clangd = {},
+        gopls = {},
+        zls = {},
+        ts_ls = {},
       }
 
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'shfmt',
+        'prettier',
+        'clang-format',
+        'gofumpt',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -834,11 +782,9 @@ require('lazy').setup {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        sh = { 'shfmt' },
+        vue = { 'prettier' },
+        javascript = { 'prettier', 'clang-format', stop_after_first = true },
       },
     },
   },
