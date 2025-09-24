3e410d1d8937a61add1a6d157bc8a2a7ddb870fe
bradybeskeen <bradonbeskeen@gmail.com>
2025-09-24 11:22:34 -0700

    Formatting fix

--- neovim/config/init.lua
+++ neovim/config/init.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.o.guifont = 'Go Mono Nerd Font:h14'
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
@@ -871,8 +872,7 @@ require('lazy').setup {
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
-  -- Highlight todo, notes, etc in comments
-  {
+  { -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
