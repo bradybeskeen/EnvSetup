44a00da753695dace5bdc78a65b16a17cbe0ad0b
bradybeskeen <bradonbeskeen@gmail.com>
2026-05-06 08:49:15 -0700

    neovim: updated zen and ide configs
    * added clipboard support for SSH sessions
    * did some general cleanup as well

--- neovim/ide/init.lua
+++ neovim/ide/init.lua
@@ -1,145 +1,97 @@
--- Set <space> as the leader key
--- See `:help mapleader`
+-- IDE config for Neovim
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.o.guifont = 'Go Mono Nerd Font:h14'
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.o` or `:help option-list`
+-- [[ Clipboard ]]
+-- Detects SSH and ensures the osc52 module exists before applying
+if vim.env.SSH_TTY then
+  local ok, osc52 = pcall(require, 'vim.ui.clipboard.osc52')
+  if ok then
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = { ['+'] = osc52.copy '+', ['*'] = osc52.copy '*' },
+      paste = { ['+'] = osc52.paste '+', ['*'] = osc52.paste '*' },
+    }
+  end
+end
 
--- Make line numbers default
+-- Use system clipboard for all yanks
+vim.o.clipboard = 'unnamedplus'
+
+-- [[ Setting Options ]]
 vim.o.number = true
 vim.o.relativenumber = true
-
--- Always show the tabline, so the buffer list is visible
 vim.o.showtabline = 2
-
--- Enable mouse mode, can be useful for resizing splits
 vim.o.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = false
+vim.o.softtabstop = 2
 
--- Set tab width to 2 spaces
-vim.o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for.
-vim.o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent.
-vim.o.expandtab = false -- Use spaces instead of tabs.
-vim.o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations.
-
--- Enable break indent
 vim.o.breakindent = true
-
--- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
-
--- Decrease update time
 vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
 
 vim.cmd.colorscheme 'lunaperche'
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
-
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
+-- [[ Keymaps ]]
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Keybinds to make split navigation easier.
---  See `:help wincmd` for a list of all window commands
+-- Window navigation
 vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Move to left window' })
 vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Move to lower window' })
 vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Move to upper window' })
 vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Move to right window' })
 
--- [[ Buffer Navigation and Management ]]
-vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = '' })
-vim.keymap.set('n', '<leader>bc', '<cmd>%bd|e#<CR>', { desc = '' })
-vim.keymap.set('n', 'H', '<cmd>bprevious<CR>', { desc = 'Previous Buffer' })
-vim.keymap.set('n', 'L', '<cmd>bnext<CR>', { desc = 'Next Buffer' })
+-- Buffer navigation
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Delete buffer' })
+vim.keymap.set('n', '<leader>bc', '<cmd>%bd|e#<CR>', { desc = 'Close other buffers' })
+vim.keymap.set('n', 'H', '<cmd>bprevious<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', 'L', '<cmd>bnext<CR>', { desc = 'Next buffer' })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
--- See `:help vim.hl.on_yank()`
+-- [[ Autocommands ]]
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
+  desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
 })
 
--- Go to last location when opening a buffer
 vim.api.nvim_create_autocmd('BufReadPost', {
   desc = 'Go to last location when opening a buffer',
   group = vim.api.nvim_create_augroup('last_loc', { clear = true }),
   pattern = '*',
   callback = function(event)
-    -- Check if the file mark is set
     local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
     local line_num = mark[1]
     local last_line = vim.api.nvim_buf_line_count(event.buf)
-
-    -- If the mark is valid (line > 0 and not past the end of the file), jump to it
     if line_num > 0 and line_num <= last_line then
       vim.api.nvim_win_set_cursor(0, mark)
     end
   end,
 })
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- [[ Plugin Manager Setup ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -148,63 +100,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     error('Error cloning lazy.nvim:\n' .. out)
   end
 end
+vim.opt.rtp:prepend(lazypath)
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
+-- [[ Configure Plugins ]]
 require('lazy').setup {
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
-  --
-
-  -- Alternatively, use `config = function() ... end` for full control over the configuration.
-  -- If you prefer to call `setup` explicitly, use:
-  --    {
-  --        'lewis6991/gitsigns.nvim',
-  --        config = function()
-  --            require('gitsigns').setup({
-  --                -- Your gitsigns configuration here
-  --            })
-  --        end,
-  --    }
-  --
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `opts` key (recommended), the configuration runs
-  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-  { -- Useful plugin to show you pending keybinds.
+  {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    event = 'VimEnter',
     opts = {
-      -- delay between pressing a key and opening which-key (milliseconds)
-      -- this setting is independent of vim.o.timeoutlen
       delay = 0,
       filter = function(mapping)
-        -- The mapping object has a 'desc' field.
-        -- If you set { desc = "..." }, mapping.desc will be "..."
-        -- If you didn't provide a 'desc', mapping.desc will be nil.
-        -- We return true only if the description is a non-empty string.
         return mapping.desc ~= nil and mapping.desc ~= ''
       end,
       icons = {
-        -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
-        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {
           Up = '<Up> ',
           Down = '<Down> ',
@@ -216,31 +125,15 @@ require('lazy').setup {
           S = '<S-…> ',
           CR = '<CR> ',
           Esc = '<Esc> ',
-          ScrollWheelDown = '<ScrollWheelDown> ',
-          ScrollWheelUp = '<ScrollWheelUp> ',
           NL = '<NL> ',
           BS = '<BS> ',
           Space = '<Space> ',
           Tab = '<Tab> ',
-          F1 = '<F1>',
-          F2 = '<F2>',
-          F3 = '<F3>',
-          F4 = '<F4>',
-          F5 = '<F5>',
-          F6 = '<F6>',
-          F7 = '<F7>',
-          F8 = '<F8>',
-          F9 = '<F9>',
-          F10 = '<F10>',
-          F11 = '<F11>',
-          F12 = '<F12>',
         },
       },
-
-      -- Document existing key chains
       spec = {
         { '<leader>t', group = 'trouble' },
-        { '<leader>c', group = 'code'},
+        { '<leader>c', group = 'code' },
         { '<leader>w', group = 'window' },
         { '<leader>b', group = 'buffer' },
         { '<leader>f', group = 'find' },
@@ -248,6 +141,5 @@ require('lazy').setup {
       },
     },
   },
-  --  Add plugins to `lua/plugins/*.lua`
   { import = 'plugins' },
 }
